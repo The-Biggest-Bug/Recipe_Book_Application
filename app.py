23cb4c4d5745674d0cf137d52f4e7c3aa1f7234c
The-Biggest-Bug <smithz3@etsu.edu>
2026-06-12 15:55:00 -0400

    Added navigation functionality to the accessible pages, added imports to app.py

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify, request, response
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home_page():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+    if request.method == 'POST':
+        #Enter functionality form API for search function
+        pass
 
+@app.route("/favorites")
+def favorites_page():
+    return render_template("favorites.html")
+
+@app.route("/new-recipes")
+def new_arrivals_page():
+    return render_template("new_recipes.html")
+
+@app.route("/suggestions")
+def suggestions_page():
+    return render_template("suggested_recipes.html")
 
 
 if __name__ == "__main__":
